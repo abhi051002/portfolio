c61108e64dfd24fba1a14da7a733fa741217c75b
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2024-03-21 21:19:46 +0530

    Cv Drive link updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/abhi051002",
   resume:
-    "https://drive.google.com/file/d/19xHMEdMziwM6-9jvqi6Qkou_CZ9s9IkY/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1wBajXzCGckENeY5N6jMYBdFZM2c41iCr/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/abhijit-nanda",
   twitter: "https://twitter.com/AbhijitNanda20",
   insta: "https://instagram.com/abhijit.nanda.969",
